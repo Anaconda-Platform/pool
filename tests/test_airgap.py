7704bdd91b80fe0a79b0e44b804ce86c17d9a18c
Jasmine Sandhu <jsandhu@anaconda.com>
2019-11-19 22:10:25 -0800

    Deleted old tests; added some tests for airgap module

---
+++ tests/test_airgap.py
@@ -0,0 +1,26 @@
+import pytest
+import os
+from ruamel_yaml import YAML
+from repo_mirror.airgap import _default_mirror_configs
+
+
+def test__default_mirror_configs_exception():
+    """tests exception raised"""
+    with pytest.raises(FileNotFoundError):
+        _default_mirror_configs("./not-exist", "DEBUG")
+
+
+def test__default_mirror_configs_jinja():
+    """test jinja parsing is correct/valid"""
+    ll = "DEBUG"
+    mirror_dir = "./mirror_dir"
+    configs = _default_mirror_configs(mirror_dir, ll)
+
+    for conf in configs:
+        assert os.path.exists(conf)
+        with open(conf, 'r') as fp:
+           yaml = YAML().load(fp)
+           assert yaml["mirror_dir"].startswith(mirror_dir)
+           assert yaml["log_dir"].startswith(mirror_dir)
+           assert yaml["log_level"] == ll
+
