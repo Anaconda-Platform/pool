7e88cf14de90b5cf75f9d9325b033e649259991b
Jasmine Sandhu <jsandhu@anaconda.com>
2019-12-01 14:29:33 -0800

    update tests to use Path

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,12 +1,17 @@
 import pytest
+from pathlib import Path
 
 
 @pytest.fixture(scope="function")
 def pkg_mirror(tmpdir):
-    channel = tmpdir.mkdir('main').mkdir('pkgs')
-    for platform in ('linux-64', 'win-64', 'osx-64'):
-        channel.mkdir(platform)
+    def _create_pkg_mirror(platforms):
+        channel = tmpdir.mkdir('main')
+        pkgs = channel.mkdir('pkgs')
+        for platform in platforms:
+            pkgs.mkdir(platform).strpath
 
-    # return channel's directory name as str:w
-    return channel.dirname
+        # return channel's directory name as str
+        return Path(channel)
+
+    return _create_pkg_mirror
 
